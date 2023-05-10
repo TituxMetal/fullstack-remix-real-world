2ff61110724ac63e9592b271aed1e0b33e14263f
Titux Metal (MBP) <github@lgdweb.fr>
2023-05-10 08:20:26 +0200

    Update remix config file

--- remix.config.js
+++ remix.config.js
@@ -3,17 +3,21 @@ const { flatRoutes } = require('remix-flat-routes')
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   cacheDirectory: './node_modules/.cache/remix',
-  ignoredRouteFiles: ['**/.*'],
+  ignoredRouteFiles: ['**/*'],
   appDirectory: 'src',
+  tailwind: true,
+  postcss: true,
+  serverModuleFormat: 'cjs',
   future: {
     v2_errorBoundary: true,
     v2_meta: true,
     v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-    unstable_tailwind: true,
-    unstable_postcss: true
+    v2_routeConvention: true
   },
   routes: async defineRoutes => {
-    return flatRoutes('routes', defineRoutes, { appDir: 'src' })
+    return flatRoutes('routes', defineRoutes, {
+      appDir: 'src',
+      ignoredRouteFiles: ['**/.*', '**/*.{test,spec}.{js,jsx,ts,tsx}']
+    })
   }
 }
