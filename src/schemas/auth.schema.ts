601eb5892e59e5838c682d78af44880d4bcc72e1
Titux Metal (MBP) <github@lgdweb.fr>
2023-05-11 08:47:44 +0200

    Add register validation schema; add a notEmpty message to getMessage function; add nonempty validation

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -10,6 +10,7 @@ const getMessage = (field: string, props?: Props) => {
 
   return {
     isEmail: `${field} must be a valid email.`,
+    notEmpty: `${field} must not be empty.`,
     minLength: `${field} must contain at least ${min} character(s).`,
     maxLength: `${field} must contain at most ${max} character(s).`
   }
@@ -18,10 +19,21 @@ const getMessage = (field: string, props?: Props) => {
 export const loginSchema = z.object({
   email: z
     .string()
+    .nonempty({ message: getMessage('Email').notEmpty })
     .email({ message: getMessage('Email').isEmail })
     .trim(),
   password: z
     .string()
     .trim()
+    .nonempty({ message: getMessage('Password').notEmpty })
     .min(6, getMessage('Password', { min: 6 }).minLength)
+})
+
+export const registerSchema = loginSchema.extend({
+  username: z
+    .string()
+    .trim()
+    .nonempty({ message: getMessage('Username').notEmpty })
+    .min(3, { message: getMessage('Username', { min: 3 }).minLength })
+    .max(15, { message: getMessage('Username', { max: 15 }).maxLength })
 })
