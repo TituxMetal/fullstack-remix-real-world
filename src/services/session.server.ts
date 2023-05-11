14ee70d0cfba19f41bff3630cac2040eabc339ab
Titux Metal (MBP) <github@lgdweb.fr>
2023-05-11 09:11:00 +0200

    Update session service; remove non used code

--- src/services/session.server.ts
+++ src/services/session.server.ts
@@ -1,97 +1,28 @@
-import { createCookieSessionStorage, redirect } from '@remix-run/node'
+import { createCookieSessionStorage } from '@remix-run/node'
 import invariant from 'tiny-invariant'
 
-import { getUserById } from '~/models'
-import type { User } from '~/models'
+const sessionSecret = process.env.SESSION_SECRET || 'supersescret'
 
-invariant(process.env.SESSION_SECRET, 'SESSION_SECRET must be set')
+invariant(
+  typeof sessionSecret === 'string',
+  `SESSION_SECRET must be set in env variables.`
+)
+
+const TEN_DAYS = 60 * 60 * 24 * 10
+const maxAge = Number(process.env.SESSION_MAX_AGE) || TEN_DAYS
+
+invariant(typeof maxAge === 'number', `MAX_AGE must be set in env variables.`)
 
 export const sessionStorage = createCookieSessionStorage({
   cookie: {
-    name: '__session',
+    name: 'rmx_real-world_session',
     httpOnly: true,
-    path: '/',
+    secure: process.env.NODE_ENV === 'production',
+    secrets: [sessionSecret],
     sameSite: 'lax',
-    secrets: [process.env.SESSION_SECRET],
-    secure: process.env.NODE_ENV === 'production'
+    path: '/',
+    maxAge
   }
 })
 
-const USER_SESSION_KEY = 'userId'
-
-export const getSession = (request: Request) => {
-  const cookie = request.headers.get('Cookie')
-  return sessionStorage.getSession(cookie)
-}
-
-export const getUserId = async (
-  request: Request
-): Promise<User['id'] | undefined> => {
-  const session = await getSession(request)
-  const userId = session.get(USER_SESSION_KEY)
-  return userId
-}
-
-export const getUser = async (request: Request) => {
-  const userId = await getUserId(request)
-  if (userId === undefined) return null
-
-  const user = await getUserById(userId)
-  if (user) return user
-
-  throw await logout(request)
-}
-
-export const requireUserId = async (
-  request: Request,
-  redirectTo: string = new URL(request.url).pathname
-) => {
-  const userId = await getUserId(request)
-  if (!userId) {
-    const searchParams = new URLSearchParams([['redirectTo', redirectTo]])
-    throw redirect(`/login?${searchParams}`)
-  }
-  return userId
-}
-
-export const requireUser = async (request: Request) => {
-  const userId = await requireUserId(request)
-
-  const user = await getUserById(userId)
-  if (user) return user
-
-  throw await logout(request)
-}
-
-export const createUserSession = async ({
-  request,
-  userId,
-  remember,
-  redirectTo
-}: {
-  request: Request
-  userId: string
-  remember: boolean
-  redirectTo: string
-}) => {
-  const session = await getSession(request)
-  session.set(USER_SESSION_KEY, userId)
-  return redirect(redirectTo, {
-    headers: {
-      'Set-Cookie': await sessionStorage.commitSession(session, {
-        maxAge: remember
-          ? 60 * 60 * 24 * 7 // 7 days
-          : undefined
-      })
-    }
-  })
-}
-
-export const logout = async (request: Request) => {
-  const session = await getSession(request)
-  return redirect('/', {
-    headers: {
-      'Set-Cookie': await sessionStorage.destroySession(session)
-    }
-  })
-}
+export const { getSession, commitSession, destroySession } = sessionStorage
