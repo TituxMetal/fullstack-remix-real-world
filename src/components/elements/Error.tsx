882dea401308e4652e1789e7b7eeff960df8b897
Titux Metal (MBP) <github@lgdweb.fr>
2023-05-11 05:51:42 +0200

    Add form elements components

---
+++ src/components/elements/Error.tsx
@@ -0,0 +1,15 @@
+import { styled } from '@slicknode/stylemapper'
+import type { HTMLAttributes } from 'react'
+
+const appErrorClassName = `mt-2 font-bold text-red-200`
+
+const StyledAppError = styled('p', appErrorClassName)
+
+const AppError = ({
+  className,
+  ...rest
+}: HTMLAttributes<HTMLParagraphElement>) => (
+  <StyledAppError className={className} {...rest} />
+)
+
+export default AppError
