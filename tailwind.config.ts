7b894075fdd75299ef9d5c8c2b434982caceb835
Titux Metal (DEV) <github@lgdweb.fr>
2024-01-13 06:17:03 +0100

    Update tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,8 @@
-import type { Config } from 'tailwindcss'
+import { type Config } from 'tailwindcss'
 
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./app/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {}
   },
